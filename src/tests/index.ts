0f03a3bdb079b864b3107ca34c69a7fcbf15bad8
PHCorra <ph.corra@gmail.com>
2023-02-14 03:53:39 -0300

    feature: add telefone methods and add to the cliente and dependente array

--- src/tests/index.ts
+++ src/tests/index.ts
@@ -1,10 +1,15 @@
 import Cliente from "../models/cliente";
 import Endereco from "../models/endereco";
+import Telefone from "../models/telefone";
 let cliente = new Cliente()
 cliente.nome = `Pedro de Alcântara João Carlos Leopoldo Salvador`
 cliente.nomeSocial = `Dom Pedro II`
 cliente.dataCadastro = new Date(1840, 6, 23)
 cliente.dataNascimento = new Date(1825, 11, 2)
+let telefonePrincipal= new Telefone()
+telefonePrincipal.ddd = `12`
+telefonePrincipal.numero = `992319662`
+cliente.telefones.push(telefonePrincipal)
 let endereco = new Endereco()
 endereco.rua = `R. do Catete`
 endereco.bairro = `Copacabana`
@@ -20,6 +25,7 @@ dependente.nomeSocial = `Princesa Isabel`
 dependente.dataCadastro = new Date(1921, 10, 14)
 dependente.dataNascimento = new Date(1846, 6, 29)
 dependente.endereco = (cliente.endereco.clonar() as Endereco)
+dependente.telefones.push(cliente.telefones[0].clonar() as Telefone)
 dependente.titular = cliente
 cliente.dependentes.push(dependente)
 
