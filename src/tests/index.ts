5ab17a8a18b28abd137d0905a6fe44da7dd3d80c
PHCorra <ph.corra@gmail.com>
2023-02-14 03:23:56 -0300

    feature: boiler plate initial of the project, based on the base project

---
+++ src/tests/index.ts
@@ -0,0 +1,27 @@
+import Cliente from "../models/cliente";
+import Endereco from "../models/endereco";
+let cliente = new Cliente()
+cliente.nome = `Pedro de Alcântara João Carlos Leopoldo Salvador`
+cliente.nomeSocial = `Dom Pedro II`
+cliente.dataCadastro = new Date(1840, 6, 23)
+cliente.dataNascimento = new Date(1825, 11, 2)
+let endereco = new Endereco()
+endereco.rua = `R. do Catete`
+endereco.bairro = `Copacabana`
+endereco.cidade = `Rio de Janeiro`
+endereco.estado = `Rio de Janeiro`
+endereco.pais = `Brasil`
+endereco.codigoPostal = `22220-000`
+cliente.endereco = endereco
+
+let dependente = new Cliente()
+dependente.nome = `Isabel Cristina Leopoldina Augusta Micaela`
+dependente.nomeSocial = `Princesa Isabel`
+dependente.dataCadastro = new Date(1921, 10, 14)
+dependente.dataNascimento = new Date(1846, 6, 29)
+dependente.endereco = (cliente.endereco.clonar() as Endereco)
+dependente.titular = cliente
+cliente.dependentes.push(dependente)
+
+console.log(cliente);
+console.log(dependente);
